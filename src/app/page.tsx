5c9fa4bbcb6ad9192523871a7a3832dfc5c6d4f8
Florbela <flor20012005@gmail.com>
2023-09-22 09:50:30 -0300

    exercício concluído

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,15 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Next.js</h1>
       <ul>
-        
+        {
+          list.map(
+            (item) => (
+              <li key={item.nome}>
+               {item.nome} - {item.idade} 
+              </li>
+            )
+          )
+        }
       </ul>
     </main>
   )
